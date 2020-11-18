59547056bdab7b3321795c89764488ef5238fbde
anurag1001001 <anuragtripathi23may@gmail.com>
2020-11-18 13:56:25 +0530

    added comment and explanation at every step

--- codeial/controllers/posts_controller.js
+++ codeial/controllers/posts_controller.js
@@ -3,6 +3,9 @@ const Post = require('../models/post');
 module.exports.create = function(req, res){
     Post.create({
         content: req.body.content,
+        // How we're fetching user._id because it's not getting from the home.ejs form only getting content so where did we're getting user._id ?
+        // Ans: remember we called a setAuthenticated functon from app.js and this function is defined in config->passport 
+        //  we only want user id not all user details that's why user._id kra hai
         user: req.user._id
     }, function(err, post){
         if (err){
--- codeial/models/post.js
+++ codeial/models/post.js
@@ -1,3 +1,5 @@
+// here we'll establish a relationship b/w databases or you can says b/w Collections
+
 const mongoose = require('mongoose');
 
 // creating a postSchema 
@@ -5,11 +7,16 @@ const postSchema = new mongoose.Schema({
     content:{
         type: String,
         required: true
+        // required: true  means that without this field we're unable to store infomation.
     },
-    // linking it to a user because specific user post something to know the user we have to link user so this is how we can link but you can explore more in the documentation
+    
     user: {
+        // linking it to a user because specific user post something to know the user we have to link user so this is how we can link but you can explore more in the documentation.
+        
+         // this type is a refering to the User collection because every post should be uniqely created by the User so this should contain unique user Objecid (that's why it is refering to objectId) to whom objectid it will store this will be indicated by ref: Collection Name.
         type: mongoose.Schema.Types.ObjectId,
-        //  refering to user schema database
+        //  refering to User schema database
+        // this is important step here we're establishing relationship b/w two different schemas(like one-to-one, one-to-many, many-to one kind of relationship).
         ref: 'User'
     }
 },
