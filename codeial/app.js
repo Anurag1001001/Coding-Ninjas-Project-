a1c77be806df3adc0a69c3ba9d492d06191bc3fe
anurag1001001 <anuragtripathi23may@gmail.com>
2020-12-07 15:33:06 +0530

    Created and Joined chatroom

--- codeial/app.js
+++ codeial/app.js
@@ -33,6 +33,14 @@ const flash = require('connect-flash');
 // to use custom middleware we must first require that middleware wherever we would be using
 const customMware = require('./config/middleware');
 
+// set up the chat server to be used with socket.io
+
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_socket').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
+
+
 
 app.use(sassMiddleware({
     src: './assets/scss',
---
+++ codeial/config/chat_socket.js
@@ -0,0 +1,35 @@
+module.exports.chatSockets = function(socketServer){
+    let io = require('socket.io')(socketServer,{
+        // I don't know what cors is but without these line in a newer version socket won't be abl e to communicate
+        cors: {
+            origin: '*',
+          }
+    });
+    //  when the connection request made by the client side this funtion will get executed
+    io.sockets.on('connection', function(socket){
+        console.log('new connection received ', socket.id);
+        
+        socket.on('disconnect', function(){
+            console.log('socket disconnected');
+        });
+
+        // .on is an socket event listener just like addEventListener in javascript
+        socket.on('join_room', function(data){
+            console.log('joining request received ', data);
+            //  Now once joinin request reeceived socket should join to that particular room.
+            //  if chatroom with this name(codeial mentioned in client side) is present then user will be entered or joiin that chatroom or if its not exist then it will create that chatroom and entered to that room.
+
+            socket.join(data.chatroom);
+
+            //  Now as i joined the room then i should tell to everyone who all already  present in that room that of my joining
+
+            //  to emit in a specific chatroom we do this
+            io.in(data.chatroom).emit('user_joined', data);
+            
+
+        });
+
+    })
+    
+
+}
